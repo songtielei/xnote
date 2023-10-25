426a392a6ffd755cc56a3d2c2e92fb28c9447189
root <songtielei@foxmail.com>
2023-10-25 15:00:44 +0800

    change

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,9 @@ import Cherry from 'cherry-markdown'
 import 'cherry-markdown/dist/cherry-markdown.min.css'
 import CherryMarkdown from './components/CherryMarkdown.vue'
 
-let showPreference = ref(false);
+let showPreference = ref(false)
 const contentList = ref<any>([])
-const workspaceList = ref<any>([]);
+const workspaceList = ref<any>([])
 
 //   const opfsRoot = await navigator.storage.getDirectory();
 // // 类型为 "directory"、名称为 "" 的 FileSystemDirectoryHandle。
@@ -45,15 +45,16 @@ const workspaceList = ref<any>([]);
 let fileHandle: FileSystemDirectoryHandle
 let contents
 let file
-let isLoad = false;
-let markdownContent = ref('');
+let isLoad = false
+let markdownContent = ref('')
 
 // file picker
 async function getFileHandle() {
   fileHandle = await window.showDirectoryPicker()
-  workspaceList.value.push(fileHandle);
-  saveHandle(fileHandle);
-  console.log(fileHandle);
+  workspaceList.value.push(fileHandle)
+  contentList.value.length = 0
+  saveHandle(fileHandle)
+  console.log(fileHandle)
   for await (const handle of fileHandle.values()) {
     if (handle.kind === 'directory') {
       continue
@@ -62,24 +63,26 @@ async function getFileHandle() {
     if (file !== null && !file.name.startsWith('.')) {
       const text = await file.text()
       //console.log(file.lastModified)
-      contentList.value.push({ content: text.substring(0, 20), dateTime: handle.lastModified, handle: handle })
+      contentList.value.push({
+        content: text.substring(0, 20),
+        dateTime: handle.lastModified,
+        handle: handle
+      })
       if (!isLoad) {
         //console.log(CherryMarkdown)
-        markdownContent.value = text;
-        isLoad = true;
+        markdownContent.value = text
+        isLoad = true
       }
-      
     }
   }
-  isLoad = false;
+  isLoad = false
 }
 async function content(handle) {
-
-  const file = await handle.getFile();
-  const text = await file.text();
+  const file = await handle.getFile()
+  const text = await file.text()
 
   //console.log(text);
-  markdownContent.value = text;
+  markdownContent.value = text
 }
 
 // write file
@@ -97,92 +100,134 @@ const items = ref([{ message: 'Foo' }, { message: 'Bar' }])
 //       marked.parse('# Marked in the browser\n\nRendered by **marked**.');
 
 async function handleScroll() {
-  console.log('scroll');
+  console.log('scroll')
 }
-function preference() {
+function preference() {}
 
+async function displayWorkspace(handle: FileSystemDirectoryHandle) {
+  console.log(handle)
+  //const request = await handle.requestPermission({})
+  const query = await handle.queryPermission({})
+  console.log('query', query)
+  // const request = await handle.requestPermission({})
+  if (query !== 'granted') {
+    const request = await handle.requestPermission({})
+    console.log('request', request)
+
+  }
+  contentList.value.length = 0
+
+for await (const h of handle.values()) {
+  if (h.kind === 'directory') {
+    continue
+  }
+  const file = await h.getFile()
+  console.log(file);
+  if (file !== null && !file.name.startsWith('.')) {
+    const text = await file.text()
+    //console.log(file.lastModified)
+    contentList.value.push({
+      content: text.substring(0, 20),
+      dateTime: h.lastModified,
+      handle: h
+    })
+    if (!isLoad) {
+      //console.log(CherryMarkdown)
+      markdownContent.value = text
+      isLoad = true
+    }
+  }
 }
-
+isLoad = false
+}
 
 const dbName = 'xzfilehandle'
-      let db
-      function openDB() {
-        const req = indexedDB.open(dbName, 1)
-        req.onupgradeneeded = (ev) => {
-          req.result.createObjectStore('handle', {
-            keyPath: 'id',
-          })
-        }
-        req.onsuccess = (ev) => {
-          db = req.result
-          restoreHandle(db)
-        }
-      }
-      openDB()
+let db
+function openDB() {
+  const req = indexedDB.open(dbName, 1)
+  req.onupgradeneeded = (ev) => {
+    req.result.createObjectStore('handle', {
+      keyPath: 'id'
+    })
+  }
+  req.onsuccess = (ev) => {
+    db = req.result
+    restoreHandle(db)
+  }
+}
+openDB()
 
-      function saveHandle(handle) {
-        const transaction = db
-          .transaction('handle', 'readwrite')
-          .objectStore('handle')
-        transaction.add({
-          id: '111',
-          handle: handle,
-        })
-        console.log('已保存 handle')
-      }
+function saveHandle(handle) {
+  const transaction = db.transaction('handle', 'readwrite').objectStore('handle')
+  transaction.add({
+    id: '111',
+    handle: handle
+  })
+  console.log('已保存 handle')
+}
 
-      function restoreHandle(db) {
-        const req = db
-          .transaction('handle', 'readwrite')
-          .objectStore('handle')
-          .get('111')
+function restoreHandle(db) {
+  const req = db.transaction('handle', 'readwrite').objectStore('handle').get('111')
 
-        req.onsuccess = (ev) => {
-          const data = req.result
-          if (data) {
-            console.log('已读取保存的 handle')
-            checkPermission(data.handle)
-          }
-        }
-      }
-      async function checkPermission(fileHandle) {
-        const query = await fileHandle.queryPermission({})
-        console.log('query', query)
-        if (query !== 'granted') {
-          const b = document.querySelector('#b')
-          b.onclick = async () => {
-            const request = await fileHandle.requestPermission({})
-            console.log('request', request)
+  req.onsuccess = (ev) => {
+    const data = req.result
+    if (data) {
+      console.log('已读取保存的 handle')
+      workspaceList.value.push(data.handle);
+    }
+  }
+}
+async function checkPermission(fileHandle) {
+  const query = await fileHandle.queryPermission({})
+  console.log('query', query)
+  workspaceList.value.push(fileHandle);
 
-            //sendFile(fileHandle, '22222,22222')
-          }
-        }
-      }
+}
 </script>
 
 <template>
   <main>
     <div class="sidebar">
       <div class="top-group">
-        <div class="workspace-item" v-for="item in workspaceList" @click=''>{{ item.name }}</div>
+        <div class="workspace-item" v-for="item in workspaceList" @click="displayWorkspace(item)">
+          {{ item.name }}
+        </div>
       </div>
       <div class="bottom-group">
-        <div @click="() => {showPreference = true}">设置</div>
+        <div
+          @click="
+            () => {
+              showPreference = true
+            }
+          "
+        >
+          设置
+        </div>
       </div>
     </div>
     <div class="nav" @scroll="handleScroll">
-      <div class="item" v-for="item in contentList" @click='content(item.handle)'>{{ item.dateTime }} {{ item.content }}</div>
+      <div class="item" v-for="item in contentList" @click="content(item.handle)">
+        {{ item.dateTime }} {{ item.content }}
+      </div>
     </div>
     <div class="content">
       <CherryMarkdown :tocVisiable="false" :value="markdownContent" />
     </div>
-
   </main>
-  <div class="preference " v-if="showPreference">
-    <div class="videoMenu" @click="() => {showPreference = false}">&times;</div>
-      选择文件夹
-      <button class="btn btn-dark mr-2" @click="getFileHandle">选择文件</button>
+  <div class="preference" v-if="showPreference">
+    <div
+      class="videoMenu"
+      @click="
+        () => {
+          showPreference = false
+        }
+      "
+    >
+      &times;
     </div>
+    选择文件夹
+    <button class="btn btn-dark mr-2" @click="getFileHandle">选择文件</button>
+  </div>
 </template>
 
 <style scoped lang="scss">
@@ -208,7 +253,6 @@ main {
     flex: 1;
     //background-color: #f5f7f9;
   }
-
 }
 .top-group {
   position: absolute;
@@ -226,17 +270,16 @@ main {
 .preference {
   display: block;
   width: 100%;
-    position: fixed;
-    z-index: 100px;
-    //right: 0px;
-    top: 0px;
-    bottom: 0px;
-    margin: 0 0 0 50px;
-    border: solid red;
-  }
-  .videoMenu {
+  position: fixed;
+  z-index: 100px;
+  //right: 0px;
+  top: 0px;
+  bottom: 0px;
+  margin: 0 0 0 50px;
+  border: solid red;
+}
+.videoMenu {
   width: 700px;
   height: 30px;
-
 }
 </style>
